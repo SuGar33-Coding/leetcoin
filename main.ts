c07fefaa6511ec88c674957d4afa82620087ec06
Gabriel Klavans <gabeklav@bu.edu>
2020-12-26 16:47:38 -0500

    Abstracted port to var

--- main.ts
+++ main.ts
@@ -4,9 +4,10 @@ import {
 } from "./controllers/test.ts";
 
 const app = opine();
+const port = 5000;
 
 // app.use(LogMiddleware).use(ErrorMiddleware);
 app
   .get("/test", thing)
-  .listen({ port: 5000 });
-console.log(`server listening on http://localhost:5000`);
+  .listen({ port });
+console.log(`server listening on http://localhost:${port}`);
