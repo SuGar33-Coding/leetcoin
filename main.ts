95e3faf5d58a8f57b54ef37207756d46823b6a2c
Gabriel Klavans <gabeklav@bu.edu>
2020-12-29 23:51:39 -0500

    Added simple request logger

--- main.ts
+++ main.ts
@@ -4,9 +4,12 @@ import router from "./routes.ts";
 const app = opine();
 const PORT = 5000;
 
-// app.use(LogMiddleware).use(ErrorMiddleware);
-app
-  .use("/api", router)
-  .listen({ port: PORT });
+// Simple log requests
+app.use((req, res, next) => {
+  console.log(`${req.method}: ${req.path}`)
+  next();
+});
+
+app.use("/api", router).listen({ port: PORT });
 
 console.log(`server listening on http://localhost:${PORT}`);
