db47969859a46a64ca3ee57ea135898d85bdc0b5
Gabriel Klavans <gabeklav@bu.edu>
2021-01-07 15:47:52 -0500

    Move FieldProp type to deps

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,4 @@
-import { FieldProps } from "https://deno.land/x/denodb@v1.0.18/lib/data-types.ts";
-import { FieldMatchingTable } from "https://deno.land/x/denodb@v1.0.18/lib/model.ts";
-import { DataTypes, Model } from "../utils/deps.ts";
+import { DataTypes, FieldProps, Model } from "../utils/deps.ts";
 
 const name: FieldProps = {
     type: DataTypes.STRING,
--- utils/deps.ts
+++ utils/deps.ts
@@ -4,13 +4,11 @@ export type {
   Response,
   Request,
   NextFunction,
-  ParamsDictionary
+  ParamsDictionary,
 } from "https://deno.land/x/opine@1.0.0/src/types.ts";
 
-export {
-  Database,
-  DataTypes,
-  Model
-} from "https://deno.land/x/denodb/mod.ts"
+export { Database, DataTypes, Model } from "https://deno.land/x/denodb/mod.ts";
+
+export type { FieldProps } from "https://deno.land/x/denodb@v1.0.18/lib/data-types.ts";
 
 export { opineCors } from "https://deno.land/x/cors/mod.ts";
