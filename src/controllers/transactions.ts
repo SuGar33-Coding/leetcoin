c4dc116b7b21d03eaaf26b6855641b88dab664a9
Gabriel Klavans <gabeklav@bu.edu>
2021-05-23 11:49:36 -0400

    Better day agg func

--- src/controllers/transactions.ts
+++ src/controllers/transactions.ts
@@ -35,50 +35,68 @@ export default {
 		return res.status(200).send(transactions);
 	},
 
-	dayEarningsAggregate: async (
-		req: Request,
-		res: Response,
-		next: NextFunction
-	) => {
+	dayEarningsAggregate: async (req: Request, res: Response) => {
+		const startDate = req.query.startDate as string;
+		const endDate = req.query.endDate as string;
+
 		const agg: any[] = [
 			{
 				$match: {
 					type: "EARNINGS",
 				},
 			},
-			{
-				$group: {
-					_id: {
-						DATE: {
-							$dateToString: {
-								format: "%m-%d-%Y",
-								date: "$createdAt",
-							},
-						},
-						NOTE: "$note",
-					},
-					count: {
-						$sum: 1,
-					},
-				},
-			},
-			{
-				$group: {
-					_id: "$_id.DATE",
-					NOTE_GROUP: {
-						$push: {
-							NOTE: "$_id.NOTE",
-							count: "$count",
-						},
-					},
-				},
-			},
-			{
-				$sort: {
-					_id: 1,
-				},
-			},
 		];
+
+		if (startDate) {
+			agg.push({
+				$match: {
+					createdAt: { $gte: new Date(startDate) },
+				},
+			});
+		}
+
+		if (endDate) {
+			agg.push({
+				$match: {
+					createdAt: { $lt: new Date(endDate) },
+				},
+			});
+		}
+
+		agg.push({
+			$group: {
+				_id: {
+					date: {
+						$dateToString: {
+							format: "%m-%d-%Y",
+							date: "$createdAt",
+						},
+					},
+					note: "$note",
+				},
+				count: {
+					$sum: 1,
+				},
+			},
+		});
+
+		agg.push({
+			$group: {
+				_id: "$_id.date",
+				note_group: {
+					$push: {
+						note: "$_id.note",
+						count: "$count",
+					},
+				},
+			},
+		});
+
+		agg.push({
+			$sort: {
+				_id: 1,
+			},
+		});
 
 		const transactions = await Transaction.aggregate(agg).exec();
 
